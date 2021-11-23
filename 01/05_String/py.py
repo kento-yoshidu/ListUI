cbe0f176559d101df8daa7714e81d0d5e6798a76
kento <kento@toriwatari.bird>
2021-11-23 14:58:43 +0900

    ✨ Update

---
+++ 01/05_String/py.py
@@ -0,0 +1,20 @@
+"""
+## 文字列の定義
+print("Hello World")
+print('Python')
+
+print("That's a Sun")
+#=> That's a Sun
+print('This is "good idea"')
+#=> This is "good idea"
+"""
+
+longstr = """Lorem ipsum dolor sit amet,
+consectetur adipisicing elit,
+sed do eiusmod tempor incididunt"""
+
+print(longstr)
+#=>Lorem ipsum dolor sit amet,
+#=>consectetur adipisicing elit,
+#=>sed do eiusmod tempor incididunt
+
