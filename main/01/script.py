1804745642c89a436e0e4a6bc57f53d9690a7273
kento <kento@toriwatari.bird>
2022-02-04 23:26:32 +0900

    ✨ Update

---
+++ main/01/script.py
@@ -0,0 +1,38 @@
+"""
+print("Hello World")
+
+# print("Hello World")
+
+print("Hello World)
+print("Hello World)
+
+msg = "Hello World"
+print(msg)
+#=> Hello World
+
+msg = "Hello Python"
+print(msg)
+#=> Hello Python
+
+print(type("Hello World"))
+#=> <type 'str'>
+
+print(type('Hello World'))
+#=> <type 'str'>
+
+print(type(1))
+#=> <type 'int'>
+
+print(type(1.0))
+#=> <type 'float'>
+
+print(type(3 + 4j))
+#=> <type 'complex'>
+
+"""
+
+print(type(True))
+#=> <type 'bool'>
+print(type(False))
+#=> <type 'bool'>
+
