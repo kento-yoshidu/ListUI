98d2b71a0b050cd671830aee182fdc51922b8fe1
kento <kento@toriwatari.bird>
2025-01-19 21:22:55 +0900

    詳細情報コンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import './App.css';
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Box, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
 
 type Data = {
   id: string
@@ -7,7 +8,50 @@ type Data = {
   summary: string
 }
 
-const ListComponent = ({ data }: { data: Data[] }) => {
+const data = [
+  {
+    id: "1",
+    title: "Rust",
+    summary: "aaaaaaaaaaaaaaaaaaaaa",
+  },
+  {
+    id: "2",
+    title: "Erlang",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+  {
+    id: "3",
+    title: "F#",
+    summary: "cccc",
+  },
+  {
+    id: "4",
+    title: "TypeScript",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+  {
+    id: "5",
+    title: "Common Lisp",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+  {
+    id: "6",
+    title: "Scala",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+  {
+    id: "7",
+    title: "Haskell",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+  {
+    id: "8",
+    title: "C++",
+    summary: "bbbbbbbbbbbbbbbbbbbbb",
+  },
+];
+
+const ListComponent = ({ data, onClick }: { data: Data[], onClick: (item: Data) => void }) => {
   return (
     <List sx={{
       margin: "15px 0",
@@ -16,6 +60,8 @@ const ListComponent = ({ data }: { data: Data[] }) => {
     }}>
       {data.map((item) => (
         <ListItem
+          key={item.id}
+          onClick={() => onClick(item)}
           sx={{
             borderBottom: "1px solid #ddd",
             transition: "0.3s",
@@ -28,54 +74,35 @@ const ListComponent = ({ data }: { data: Data[] }) => {
           <ListItemText primary={item.title} />
         </ListItem>
       ))}
-
     </List>
   )
 }
 
+const Summary = ({ selectedItem }: { selectedItem: Data | null }) => {
+  return (
+    <Box
+      sx={{
+        flex: 1,
+        marginTop: 2,
+        padding: 2,
+        border: "1px solid #ddd",
+        borderRadius: 1,
+        backgroundColor: "white",
+        minHeight: "10vh",
+      }}
+    >
+      {selectedItem && (
+        <Paper sx={{ padding: 2 }}>
+          <Typography variant="h6">{selectedItem.title}</Typography>
+          <Typography variant="body1">{selectedItem.summary}</Typography>
+        </Paper>
+      )}
+    </Box>
+  )
+}
+
 function App() {
-  const data = [
-    {
-      id: "1",
-      title: "Rust",
-      summary: "aaaaaaaaaaaaaaaaaaaaa",
-    },
-    {
-      id: "2",
-      title: "Erlang",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-    {
-      id: "3",
-      title: "F#",
-      summary: "cccc",
-    },
-    {
-      id: "4",
-      title: "TypeScript",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-    {
-      id: "5",
-      title: "Common Lisp",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-    {
-      id: "6",
-      title: "Scala",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-    {
-      id: "7",
-      title: "Haskell",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-    {
-      id: "8",
-      title: "C++",
-      summary: "bbbbbbbbbbbbbbbbbbbbb",
-    },
-  ];
+  const [selectedItem, setSelectedItem] = useState<Data | null>(null);
 
   return (
     <Box sx={{
@@ -95,7 +122,43 @@ function App() {
         List
       </Typography>
 
-      <ListComponent data={data} />
+      <Typography
+        variant="h2"
+        sx={{
+          margin: "10px 0",
+          fontSize: 18,
+          fontWeight: "bold",
+        }}
+      >
+        {selectedItem?.title}
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+        }}
+      >
+        <Box
+          sx={{
+            flex: selectedItem ? 1 : 2,
+            transition: "0.3s",
+          }}
+        >
+          <ListComponent data={data} onClick={setSelectedItem} />
+        </Box>
+
+        {selectedItem && (
+          <Box
+            sx={{
+              flex: 1,
+              transition: "0.3s",
+            }}
+          >
+            <Summary selectedItem={selectedItem} />
+          </Box>
+        )}
+      </Box>
     </Box>
   )
 }
