544031dd052055c1a85856e36df6cece21e5f975
kento <kento@toriwatari.bird>
2025-01-19 21:04:26 +0900

    ⚡ リストを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+
+type Data = {
+  id: string
+  title: string
+  summary: string
+}
+
+const ListComponent = ({ data }: { data: Data[] }) => {
+  return (
+    <List sx={{
+      margin: "15px 0",
+      padding: 0,
+      border: "1px solid #aaa",
+    }}>
+      {data.map((item) => (
+        <ListItem
+          sx={{
+            borderBottom: "1px solid #ddd",
+            transition: "0.3s",
+            "&:hover": {
+              backgroundColor: "#f0f0f0",
+              cursor: "pointer",
+            }
+          }}
+        >
+          <ListItemText primary={item.title} />
+        </ListItem>
+      ))}
+
+    </List>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+    {
+      id: "1",
+      title: "Rust",
+      summary: "aaaaaaaaaaaaaaaaaaaaa",
+    },
+    {
+      id: "2",
+      title: "Erlang",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+      id: "3",
+      title: "F#",
+      summary: "cccc",
+    },
+    {
+      id: "4",
+      title: "TypeScript",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+      id: "5",
+      title: "Common Lisp",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+      id: "6",
+      title: "Scala",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+      id: "7",
+      title: "Haskell",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+      id: "8",
+      title: "C++",
+      summary: "bbbbbbbbbbbbbbbbbbbbb",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{
+      width: "95%",
+      height: "80vh",
+      margin: "9vh auto",
+      padding: 2,
+      border: "1px solid #aaa",
+    }}>
+      <Typography
+        variant="h1"
+        sx={{
+          fontSize: 30,
+          fontWeight: "bold",
+        }}
+      >
+        List
+      </Typography>
+
+      <ListComponent data={data} />
+    </Box>
   )
 }
 
